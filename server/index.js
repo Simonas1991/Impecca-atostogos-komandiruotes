6930888a206e4cd10437cbcf70af2660e2272076
Simonas1991 <simas.sniuksta@gmail.com>
2021-05-31 14:24:15 +0300

    feat: mongoose install, connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const morgan = require('morgan');
 require('dotenv').config({ path: 'server/.env' });
 const workersRouter = require('./routes/workers');
+const mongoose = require('mongoose')
 
 const server = express();
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, SERVER_URL } = process.env;
 
 // middleware
 server.use(cors());
@@ -13,13 +14,22 @@ server.use(morgan('tiny'));
 server.use(express.json());
 
 // Additional routes
-
 server.use('/workers', workersRouter)
 
+// Main route
 server.get('/', (req, res) => {
     res.send('Serveris veikia')
 });
 
-server.listen(SERVER_PORT, () => {
-    console.log(`Impecca server is running on http://localhost:${SERVER_PORT}`)
+// mongoDB connection
+mongoose.connect(SERVER_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('connected', function () {
+    console.log('Connected to Database successfully!');
+    server.listen(SERVER_PORT, () => {
+        console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+    });
 });
+
+db.on('error', (error) => console.error('ERROR: FAILED TO CONNECT TO DB:\n ' + error));
